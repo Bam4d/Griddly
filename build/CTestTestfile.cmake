90b9ea26a2434da9a6822ec794d7f169cf8c2eca
Bam4d <chrisbam4d@gmail.com>
2019-12-16 16:33:18 +0000

    remove IDE specifics and build dir

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/bam4d/qmul/griddy
-# Build directory: /home/bam4d/qmul/griddy/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
