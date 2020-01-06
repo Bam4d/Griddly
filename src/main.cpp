8645e7887cf5f77cf2ba074bca756ee154161a60
Bam4d <chrisbam4d@gmail.com>
2020-01-06 16:26:41 +0000

    adding google tests so we can start writing unit tests

--- src/main.cpp
+++ src/main.cpp
@@ -14,23 +14,22 @@ int main(int, char**) {
 
   griddy::TileObserver tileObserver = griddy::TileObserver();
 
-  std::shared_ptr<griddy::Resource> testResource = std::shared_ptr<griddy::Resource>(new griddy::Resource(10));
-
-  griddy::GridLocation location = testResource->getLocation();
-
-  std::cout << location.x << std::endl;
-  std::cout << location.y << std::endl;
+  std::shared_ptr<griddy::Resource> testResource1 = std::shared_ptr<griddy::Resource>(new griddy::Resource(10));
+  std::shared_ptr<griddy::Resource> testResource2 = std::shared_ptr<griddy::Resource>(new griddy::Resource(10));
+  std::shared_ptr<griddy::Resource> testResource3 = std::shared_ptr<griddy::Resource>(new griddy::Resource(10));
 
   std::shared_ptr<griddy::Grid> grid = std::shared_ptr<griddy::Grid>(new griddy::Grid(10, 10));
 
-  grid->initObject({5,5}, std::move(testResource));
+  grid->initObject({0,0}, std::move(testResource1));
+  grid->initObject({5,5}, std::move(testResource2));
+  grid->initObject({8,7}, std::move(testResource3));
 
   std::unique_ptr<uint8_t[]> observation = tileObserver.observe(grid);
 
   tileObserver.print(std::move(observation), grid);
 
   std::vector<std::shared_ptr<griddy::Action>> actions = std::vector<std::shared_ptr<griddy::Action>>();
-  std::shared_ptr<griddy::MoveAction> moveAction = std::shared_ptr<griddy::MoveAction>(new griddy::MoveAction(griddy::Direction::UP, location));
+  std::shared_ptr<griddy::MoveAction> moveAction = std::shared_ptr<griddy::MoveAction>(new griddy::MoveAction(griddy::Direction::UP, {0,0}));
 
   actions.push_back(std::move(moveAction));
 
