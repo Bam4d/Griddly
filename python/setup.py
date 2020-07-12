dcf26e107413c421097d7c69069d578666b179f7
Bam4d <chrisbam4d@gmail.com>
2020-07-12 21:16:27 +0100

    changing version numbers

--- python/setup.py
+++ python/setup.py
@@ -77,7 +77,7 @@ def griddly_package_data(config='Debug'):
 
 setup(
     name='griddly',
-    version="0.0.7",
+    version="0.0.8",
     author_email="chrisbam4d@gmail.com",
     description="Griddly Python Libraries",
     long_description=long_description,
