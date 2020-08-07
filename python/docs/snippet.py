a139c2d8a4ec52746cb3083b1a972ae04d86b8c9
Bam4d <chrisbam4d@gmail.com>
2020-08-07 09:58:16 +0100

    add env.reset() to example

--- python/docs/snippet.py
+++ python/docs/snippet.py
@@ -1,13 +1,18 @@
-
 import gym
 from griddly import GymWrapperFactory, gd
 
 if __name__ == '__main__':
     wrapper = GymWrapperFactory()
 
-    environment_name = 'sokoban'
-    level = 2
+    wrapper.build_gym_from_yaml(
+        'Sokoban',
+        'Single-Player/GVGAI/sokoban.yaml',
+        player_observer_type=gd.ObserverType.VECTOR,
+        global_observer_type=gd.ObserverType.SPRITE_2D,
+        level=2
+    )
 
-    wrapper.build_gym_from_yaml(environment_name, f'Single-Player/GVGAI/{environment_name}.yaml', player_observer_type=gd.ObserverType.SPRITE_2D, level=level)
+    env = gym.make(f'GDY-Sokoban-v0')
+    env.reset()
 
-    env = gym.make(f'GDY-{environment_name}-v0')
+    env.render(observer='global')
