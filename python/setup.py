1f7f7dbf5f3af0b1eaa6861e9025fd637d06264d
Bam4d <chrisbam4d@gmail.com>
2020-08-10 16:55:07 +0100

    bumping version number to test distribution

--- python/setup.py
+++ python/setup.py
@@ -71,7 +71,7 @@ def griddly_package_data(config='Debug'):
 
 setup(
     name='griddly',
-    version="0.0.13",
+    version="0.0.14",
     author_email="chrisbam4d@gmail.com",
     description="Griddly Python Libraries",
     long_description=long_description,
