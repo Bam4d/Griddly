14dfcca6ab6080c2b0db0e18a659dbba34b39542
Bam4d <chrisbam4d@gmail.com>
2020-08-10 20:32:08 +0100

    fe more fixes

--- python/docs/snippet.py
+++ python/docs/snippet.py
@@ -1,25 +1,15 @@
 import gym
-import numpy as np
 from griddly import GymWrapperFactory, gd
 
 if __name__ == '__main__':
     wrapper = GymWrapperFactory()
 
     wrapper.build_gym_from_yaml(
-        'Spiders-Adv',
-        'Single-Player/Mini-Grid/minigrid-spiders.yaml',
-        level=0,
-        global_observer_type=gd.ObserverType.SPRITE_2D,
+        'Sokoban-Adv',
+        'Single-Player/GVGAI/sokoban.yaml',
         player_observer_type=gd.ObserverType.SPRITE_2D,
-        tile_size=100
+        level=2
     )
 
-    env = gym.make('GDY-Spiders-Adv-v0')
+    env = gym.make('GDY-Sokoban-Adv-v0')
     env.reset()
-
-    # Replace with your own control algorithm!
-    for s in range(1000):
-        obs, reward, done, info = env.step(env.action_space.sample())
-        env.render()
-
-        env.render(observer='global')
