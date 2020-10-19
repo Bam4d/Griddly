f2da393b8d5fc3dfc63889914a78c36b41133364
Bam4d <chrisbam4d@gmail.com>
2020-10-19 17:52:17 +0100

    release with fixes for tile size

--- python/setup.py
+++ python/setup.py
@@ -71,7 +71,7 @@ def griddly_package_data(config='Debug'):
 
 setup(
     name='griddly',
-    version="0.1.5",
+    version="0.1.6",
     author_email="chrisbam4d@gmail.com",
     description="Griddly Python Libraries",
     long_description=long_description,
