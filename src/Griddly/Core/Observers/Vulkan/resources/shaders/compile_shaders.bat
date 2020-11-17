a4cce0ea85149c552f977519dc5860baf0464218
Bam4d <chrisbam4d@gmail.com>
2020-11-17 14:43:41 +0000

    more syntax testing

--- src/Griddly/Core/Observers/Vulkan/resources/shaders/compile_shaders.bat
+++ src/Griddly/Core/Observers/Vulkan/resources/shaders/compile_shaders.bat
@@ -1,13 +1,13 @@
-set SHADER_OUTPUT_DIR=../../../../../../../resources/shaders
+set SHADER_OUTPUT_DIR=..\..\..\..\..\..\..\resources\shaders
 
 echo %SHADER_OUTPUT_DIR%
-echo %SHADER_OUTPUT_DIR%/triangle.frag.spv
+echo %SHADER_OUTPUT_DIR%\triangle.frag.spv
 
 
 if not exist "%SHADER_OUTPUT_DIR%" (mkdir %SHADER_OUTPUT_DIR%)
 
-glslc triangle.frag -o %SHADER_OUTPUT_DIR%/triangle.frag.spv
-glslc triangle.vert -o %SHADER_OUTPUT_DIR%/triangle.vert.spv
+glslc triangle.frag -o %SHADER_OUTPUT_DIR%\triangle.frag.spv
+glslc triangle.vert -o %SHADER_OUTPUT_DIR%\triangle.vert.spv
 
-glslc triangle-textured.frag -o %SHADER_OUTPUT_DIR%/triangle-textured.frag.spv
-glslc triangle-textured.vert -o %SHADER_OUTPUT_DIR%/triangle-textured.vert.spv
+glslc triangle-textured.frag -o %SHADER_OUTPUT_DIR%\triangle-textured.frag.spv
+glslc triangle-textured.vert -o %SHADER_OUTPUT_DIR%\triangle-textured.vert.spv
