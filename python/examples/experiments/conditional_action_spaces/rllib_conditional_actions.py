2a5784c2bb6f56d337dc803d17dd6bfe17a66a57
Bam4d <chrisbam4d@gmail.com>
2021-03-22 14:23:56 +0000

    num cpus has to be independent of num workers as one cpu is used for impala

--- python/examples/experiments/conditional_action_spaces/rllib_conditional_actions.py
+++ python/examples/experiments/conditional_action_spaces/rllib_conditional_actions.py
@@ -22,7 +22,8 @@ parser.add_argument('--yaml-file', help='YAML file condining GDY for the game')
 
 parser.add_argument('--root-directory', default=os.path.expanduser("~/ray_results"),
                     help='root directory for all data associated with the run')
-parser.add_argument('--num-gpus', default=1, type=int, help='Number of GPUs to make available')
+parser.add_argument('--num-gpus', default=1, type=int, help='Number of GPUs to make available to ray.')
+parser.add_argument('--num-cpus', default=8, type=int, help='Number of CPUs to make available to ray.')
 
 parser.add_argument('--num-workers', default=11, type=int, help='Number of workers')
 parser.add_argument('--num-envs-per-worker', default=5, type=int, help='Number of workers')
@@ -43,7 +44,7 @@ if __name__ == '__main__':
     sep = os.pathsep
     os.environ['PYTHONPATH'] = sep.join(sys.path)
 
-    ray.init(include_dashboard=False, num_gpus=args.num_gpus, num_cpus=args.num_workers)
+    ray.init(include_dashboard=False, num_gpus=args.num_gpus, num_cpus=args.num_cpus)
     #ray.init(include_dashboard=False, num_gpus=1, local_mode=True)
 
     env_name = "ray-griddly-env"
