d6688be214b1f5f2dc310642b2f80138aa1dc249
Chris Bamford <chrisbam4d@gmail.com>
2021-10-05 17:26:03 +0100

    Update Clang-cxx-dev-tools.cmake
    dont want to recurse everything on the HD

--- cmake/settings/Clang-cxx-dev-tools.cmake
+++ cmake/settings/Clang-cxx-dev-tools.cmake
@@ -1,9 +1,13 @@
 file(GLOB_RECURSE
         ALL_CXX_SOURCE_FILES
-        ${PROJ_SOURCES_DIR}/**.[ch]pp
-        ${PROJ_SOURCES_DIR}/**.h
+        ${GRIDDLY_SRC_DIR}/*.[ch]pp
+        ${GRIDDLY_SRC_DIR}/*.h
+        ${GRIDDLY_TEST_DIR}/*.[ch]pp
+        ${GRIDDLY_TEST_DIR}/*.h
         )
+
 # Adding clang-format target if executable is found
+message(STATUS "finding clang format")
 find_program(CLANG_FORMAT "clang-format")
 if(CLANG_FORMAT)
     add_custom_target(
