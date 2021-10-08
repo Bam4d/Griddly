50657faf74d26f6656122435990873149843a4cf
Bam4d <chrisbam4d@gmail.com>
2021-10-08 17:04:04 +0100

    trying to get manylinux to recognise conan

--- python/manylinux/build-wheels.sh
+++ python/manylinux/build-wheels.sh
@@ -9,6 +9,8 @@ yum install -y vulkan vulkan-devel ninja-build
 # Get Dependencies
 /opt/python/$PYBIN/bin/pip install cmake setuptools wheel twine conan
 
+export PATH=$PATH:/opt/python/$PYBIN/bin
+
 # Download and build glslc for manylinux
 # VULKAN SDK does not support manylinux so cannot use the glslc version from the sdk
 # https://vulkan.lunarg.com/issue/home?limit=10;q=;mine=false;org=false;khronos=false;lunarg=false;indie=false;status=new,open
@@ -22,12 +24,12 @@ yum install -y vulkan vulkan-devel ninja-build
 
 # Cmake Build Griddly
 cd /opt/Griddly
-/opt/python/$PYBIN/bin/cmake -E make_directory build
+cmake -E make_directory build
 cd build
-/opt/python/$PYBIN/bin/cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python -DPYBIND11_PYTHON_VERSION=$PYVERSION
-/opt/python/$PYBIN/bin/cmake --build . --target python_griddly
+cmake .. -GNinja -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python -DPYBIND11_PYTHON_VERSION=$PYVERSION
+cmake --build . --target python_griddly
 
 # Create Wheel
 cd python
-/opt/python/$PYBIN/bin/python setup.py bdist_wheel --plat $PLAT
+python setup.py bdist_wheel --plat $PLAT
 
