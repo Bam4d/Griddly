51cbf2399a9f2925d7c7af8a428365ad36448fa0
Bam4d <chrisbam4d@gmail.com>
2021-10-08 16:27:49 +0100

    fixing deploy scripts

--- python/manylinux/build-wheels.sh
+++ python/manylinux/build-wheels.sh
@@ -7,18 +7,18 @@ set -o pipefail
 yum install -y vulkan vulkan-devel ninja-build
 
 # Get Dependencies
-/opt/python/$PYBIN/bin/pip install cmake setuptools wheel twine
+/opt/python/$PYBIN/bin/pip install cmake setuptools wheel twine conan
 
 # Download and build glslc for manylinux
 # VULKAN SDK does not support manylinux so cannot use the glslc version from the sdk
 # https://vulkan.lunarg.com/issue/home?limit=10;q=;mine=false;org=false;khronos=false;lunarg=false;indie=false;status=new,open
-cd /opt/
-git clone https://github.com/google/shaderc
-cd shaderc/
-/opt/python/$PYBIN/bin/python ./utils/git-sync-deps
-/opt/python/$PYBIN/bin/cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python
-/opt/python/$PYBIN/bin/cmake --build . --target glslc_exe
-ln -s /opt/shaderc/glslc/glslc /usr/bin/glslc
+# cd /opt/
+# git clone https://github.com/google/shaderc
+# cd shaderc/
+# /opt/python/$PYBIN/bin/python ./utils/git-sync-deps
+# /opt/python/$PYBIN/bin/cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python
+# /opt/python/$PYBIN/bin/cmake --build . --target glslc_exe
+# ln -s /opt/shaderc/glslc/glslc /usr/bin/glslc
 
 # Cmake Build Griddly
 cd /opt/Griddly
