d77de6e22c7df96408c85a3d507b48fda9c41c50
Bam4d <chrisbam4d@gmail.com>
2021-11-15 20:21:25 +0000

    bumping version numbers

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Chris Bamford'
 author = 'Chris Bamford'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.13'
+release = '1.2.14'
 
 
 # -- General configuration ---------------------------------------------------
