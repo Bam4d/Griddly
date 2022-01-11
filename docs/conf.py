3587a9cb7031bd8d8eb7b7f30153cf577466788e
Bam4d <chrisbam4d@gmail.com>
2022-01-11 14:38:16 +0000

    updating version numbers, licence

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Chris Bamford'
 author = 'Chris Bamford'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.23'
+release = '1.2.24'
 
 
 # -- General configuration ---------------------------------------------------
