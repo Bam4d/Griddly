5ab9f9aa49f47965bc50f05db4f182b97f9e0a02
Bam4d <chrisbam4d@gmail.com>
2022-01-12 13:59:11 +0000

    cherrypick

---
+++ python/tools/flamegraph/shader_benchmark/benchmark.py
@@ -0,0 +1,30 @@
+from griddly import GymWrapperFactory, gd, GymWrapper
+import timeit
+
+if __name__ == '__main__':
+    wrapper = GymWrapperFactory()
+
+    env = GymWrapper('Single-Player/GVGAI/sokoban.yaml',
+                     player_observer_type=gd.ObserverType.SPRITE_2D,
+                     global_observer_type=gd.ObserverType.SPRITE_2D,
+                     level=0)
+
+    env.reset()
+
+    start = timeit.default_timer()
+
+    frames = 0
+
+    for i in range(200000):
+
+        obs, reward, done, info = env.step(env.action_space.sample())
+
+        frames += 1
+
+        if done:
+
+            end = timeit.default_timer()
+            print(f'{frames/(end - start)} SPS')
+            frames = 0
+            env.reset()
+            start = timeit.default_timer()
