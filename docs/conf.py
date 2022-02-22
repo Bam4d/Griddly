f0347a02abcd3299ba2b4d40d3d4483c5900d6b5
Bam4d <chrisbam4d@gmail.com>
2022-02-22 10:11:56 +0000

    actually push version changes and docs fixes

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Chris Bamford'
 author = 'Chris Bamford'
 
 # The full version, including alpha/beta/rc tags
-release = '1.2.35'
+release = '1.2.36'
 
 
 # -- General configuration ---------------------------------------------------
