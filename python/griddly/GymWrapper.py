29f7323517e0ee1d22ee6d0393fc70e87ee156fd
Bam4d <chrisbam4d@gmail.com>
2022-03-29 12:11:13 +0100

    fixing tests

--- python/griddly/GymWrapper.py
+++ python/griddly/GymWrapper.py
@@ -110,6 +110,8 @@ class GymWrapper(gym.Env):
 
     def _get_observer_name(self, observer_type_or_string):
         if isinstance(observer_type_or_string, gd.ObserverType):
+            if observer_type_or_string.name == 'ASCII':
+                return observer_type_or_string.name
             return observer_type_or_string.name.title().replace('_','')
         else:
             return observer_type_or_string
