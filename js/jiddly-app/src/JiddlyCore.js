57646130a36d1d5859aa4e9caa3eb1cca00dfb51
Bam4d <chrisbam4d@gmail.com>
2022-04-01 14:10:02 +0100

    converting into react project so its easier to use with components etc

---
+++ js/jiddly-app/src/JiddlyCore.js
@@ -0,0 +1,25 @@
+
+
+/**
+ * Contains all the code for loading and interfacing with Jiddly
+ */
+class JiddlyCore {
+
+    constructor() {
+    }
+
+    init = async () => {
+    }
+
+    getPlayerObservations = () => {
+
+    }
+
+    getState = () => {
+
+    }
+
+
+}
+
+export default JiddlyCore;
---
+++ js/jiddly-app/src/JiddlyRenderer.js
@@ -0,0 +1,12 @@
+import { Component } from "react";
+
+
+class JiddlyRenderer extends Component {
+    constructor(props) {
+
+    }
+
+
+};
+
+export default JiddlyRenderer;
