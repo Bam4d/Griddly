f4235691ceff553a7cef64e894a1a67399f3c24d
Ville Kopio <ville.kopio@gmail.com>
2022-07-18 21:54:50 +0300

    Add shader compilation scripts that compile also test shaders

---
+++ compile_shaders.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+compile_shaders_in_dir () {
+  glslc $1/triangle-textured.frag -o $1/triangle-textured.frag.spv
+  glslc $1/triangle-textured.vert -o $1/triangle-textured.vert.spv
+}
+
+compile_shaders_in_dir ./resources/shaders
+compile_shaders_in_dir ./tests/resources/observer/block/shaders/global_lighting
+compile_shaders_in_dir ./tests/resources/observer/isometric/shaders/lighting
+compile_shaders_in_dir ./tests/resources/observer/sprite/shaders/health_bars
