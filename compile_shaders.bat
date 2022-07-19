ebb48d7d0e8cae0b8a44ac2d1f5779302858f1a7
Bam4d <chrisbam4d@gmail.com>
2022-07-19 18:53:50 +0100

    shader compilation working on windows

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -1,9 +1,12 @@
+cd /D "%~dp0"
+
 CALL :compile_shaders_in_dir .\resources\shaders
 CALL :compile_shaders_in_dir .\tests\resources\observer\block\shaders\global_lighting
 CALL :compile_shaders_in_dir .\tests\resources\observer\isometric\shaders\lighting
 CALL :compile_shaders_in_dir .\tests\resources\observer\sprite\shaders\health_bars
 
-:compile_shaders_in_dir
-  glslc %~1\triangle-textured.frag -o %~1\triangle-textured.frag.spv
-  glslc %~1\triangle-textured.vert -o %~1\triangle-textured.vert.spv
 EXIT /B 0
+
+:compile_shaders_in_dir
+glslc %~1\triangle-textured.frag -o %~1\triangle-textured.frag.spv
+glslc %~1\triangle-textured.vert -o %~1\triangle-textured.vert.spv
