799ed389907ce3b3d1ba002f7a4710fbb1fd4abe
Bam4d <chrisbam4d@gmail.com>
2022-07-20 09:57:42 +0100

    bumping to version 1.4.2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Chris Bamford'
 author = 'Chris Bamford'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.1'
+release = '1.4.2'
 
 
 # -- General configuration ---------------------------------------------------
