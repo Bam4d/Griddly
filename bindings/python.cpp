ad1f83a7395056e70965b5b0ab041de079434597
Bam4d <chrisbam4d@gmail.com>
2022-10-04 13:47:16 +0100

    bumping versions

--- bindings/python.cpp
+++ bindings/python.cpp
@@ -12,7 +12,7 @@ namespace griddly {
 
 PYBIND11_MODULE(python_griddly, m) {
   m.doc() = "Griddly python bindings";
-  m.attr("version") = "1.5.0";
+  m.attr("version") = "1.5.1";
 
 #ifndef NDEBUG
   spdlog::set_level(spdlog::level::debug);
