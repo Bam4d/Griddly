2421a367af171a31ce1d18d131f81ad43b2e0349
Bam4d <chrisbam4d@gmail.com>
2022-10-28 19:24:34 +0100

    fixing wheel build

--- python/manylinux/build-wheels.sh
+++ python/manylinux/build-wheels.sh
@@ -14,13 +14,12 @@ export CONAN_SYSREQUIRES_SUDO=0
 
 export PATH=$PATH:/opt/python/$PYBIN/bin
 
-# conan install /opt/Griddly/deps --build=shaderc
 
 echo "Conan Build Finished"
 
-# # Cmake Build Griddlycmake . -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR:STRING=$(py.pythonLocation)
+# # Cmake Build Griddly
 cd /opt/Griddly
-/opt/python/$PYBIN/bin/cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR:STRING=/opt/python/$PYBIN/ -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python -S /opt/Griddly -B /opt/Griddly/build_manylinux
+/opt/python/$PYBIN/bin/cmake . -GNinja -DCMAKE_BUILD_TYPE=Release -DPython_ROOT_DIR:STRING=/opt/python/$PYBIN -DPYTHON_EXECUTABLE:FILEPATH=/opt/python/$PYBIN/bin/python -S /opt/Griddly -B /opt/Griddly/build_manylinux
 /opt/python/$PYBIN/bin/cmake --build /opt/Griddly/build_manylinux --target python_griddly
 
 # # # Create Wheel
