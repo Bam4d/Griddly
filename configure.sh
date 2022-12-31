db9de46c0be6cdf35245c38d0c29fe580c0e9444
Bam4d <chrisbam4d@gmail.com>
2022-12-31 13:25:52 +0000

    adding documentation for building using conan and presets

--- configure.sh
+++ configure.sh
@@ -30,11 +30,7 @@ echo $PLATFORM
 if [[ $PLATFORM == "WASM" ]]
 then
   conan install deps/wasm/conanfile_wasm.txt --profile:host deps/wasm/emscripten.profile --profile:build default -s build_type=$BUILD --build missing -if build_wasm
-  cmake . -B build_wasm -GNinja -DWASM=ON -DCMAKE_BUILD_TYPE=$BUILD -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-  cmake --build build_wasm --config $BUILD
 else
   conan install deps/conanfile.txt --profile default --profile deps/build.profile -s build_type=$BUILD --build $CONAN_BUILD -if build
-  cmake . -B build -GNinja -DCMAKE_BUILD_TYPE=$BUILD -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake
-  cmake --build build --config $BUILD
 fi
 
