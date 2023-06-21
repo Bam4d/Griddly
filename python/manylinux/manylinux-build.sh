ae1ea471a314682237febe99a6efdd2480c43fe7
Bam4d <chrisbam4d@gmail.com>
2023-06-21 20:43:56 +0100

    dont need these manylinux build files anymore, also run the tests without the Observer things that crash

--- python/manylinux/manylinux-build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-docker run -e PYVERSION=$PYVERSION -e PYBIN=$PYBIN -e PLAT=$PLATFORM -v "$GRIDDLY_ROOT:/opt/Griddly" quay.io/pypa/$PLATFORM /opt/Griddly/python/manylinux/build.sh
--- python/manylinux/test.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /opt/Griddly
-ctest --test-dir /opt/Griddly/build_manylinux
