1dd4583768b3deee0ed5d6666df85c8b5a0ce50f
Michael Aichmueller <m.aichmueller@gmail.com>
2023-06-22 19:07:08 +0200

    adapt rng generation to use boost and set up reproducibility unit test
    pray that boost's distributions are cross-platform

--- src/Griddly/Core/Util/RandomGenerator.cpp
+++ src/Griddly/Core/Util/RandomGenerator.cpp
@@ -1,5 +1,8 @@
 #include "RandomGenerator.hpp"
 
+#include "boost/random.hpp"
+
+
 namespace griddly {
 
 
@@ -7,13 +10,13 @@ void RandomGenerator::seed(int32_t seed) {
   randomGenerator_.seed(seed);
 }
 
-const int32_t RandomGenerator::sampleInt(int32_t min, int32_t max) {
-  std::uniform_int_distribution<int32_t> dist(min, max);
+int32_t RandomGenerator::sampleInt(int32_t min, int32_t max) {
+  boost::uniform_int<int32_t> dist(min, max);
   return dist(randomGenerator_);
 }
 
-const float RandomGenerator::sampleFloat(float min, float max) {
-  std::uniform_real_distribution<float> dist(min, max);
+float RandomGenerator::sampleFloat(float min, float max) {
+    boost::uniform_real<float> dist(min, max);
   return dist(randomGenerator_);
 }
 
