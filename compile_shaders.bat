381ba5da4abf088b82abf7d6e0462b1e1f10e4a2
Bam4d <chrisbam4d@gmail.com>
2023-12-24 21:59:50 +0000

    fixing tiny error

--- compile_shaders.bat
+++ compile_shaders.bat
@@ -7,7 +7,7 @@ CALL :compile_shaders_in_dir .\tests\resources\observer\block\shaders\global_lig
 CALL :compile_shaders_in_dir .\tests\resources\observer\isometric\shaders\lighting
 CALL :compile_shaders_in_dir .\tests\resources\observer\sprite\shaders\health_bars
 
-CALL :compile_shaders_in_dir ".\python\examples\Custom Shaders\Global Lighting\shader"
+CALL :compile_shaders_in_dir ".\python\examples\Custom Shaders\Global Lighting\shaders"
 CALL :compile_shaders_in_dir ".\python\examples\Custom Shaders\Health Bars\shaders"
 CALL :compile_shaders_in_dir ".\python\examples\Custom Shaders\Object Lighting\shaders"
 
